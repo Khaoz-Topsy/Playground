08ff9e3a5dc831f2468f5d68a44af4a5766a57e7
Kurt Lourens <49388465+KurtLourens-Work@users.noreply.github.com>
2021-06-22 11:28:45 +0200

    more improvements

--- src/components/applet/settings/settingItemSection.tsx
+++ src/components/applet/settings/settingItemSection.tsx
@@ -10,14 +10,16 @@ interface ISettingItemSection {
 export const SettingItemSection: React.FunctionComponent<ISettingItemSection> = props => {
     return (
         <Box mb={6}>
-            <Text fontSize="2xl" >{props.heading}</Text>
+            <Text fontSize="2xl" className="noselect">{props.heading}</Text>
             {
                 (props.showDivider ?? true) &&
                 <Divider mt={2} mb={2} />
             }
             {
                 props.subTexts &&
-                props.subTexts.map((text, index) => <Text key={index} mb={1} fontSize="md" color={'whiteAlpha.600'}>{text}</Text>)
+                props.subTexts.map((text, index) => (
+                    <Text key={index} mb={1} fontSize="md" color={'whiteAlpha.600'} className="noselect">{text}</Text>
+                ))
             }
             <Box mt={1}>{props.children}</Box>
         </Box>
