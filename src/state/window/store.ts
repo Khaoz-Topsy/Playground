2b6dc7d6355a191e6be2a3801746854f38bbf0ec
Kurt Lourens <hi@kurtlourens.com>
2023-01-16 17:23:34 +0100

    💚 Fix build

--- src/state/window/store.ts
+++ src/state/window/store.ts
@@ -1,7 +1,5 @@
 import { Store } from "pullstate";
-import { AppletType } from "../../constants/enum/appletType";
 import { LaunchedApp } from "../../contracts/launchedApp";
-import { LocaleKey } from "../../localization/LocaleKey";
 
 export interface IWindowStore {
     currentFocused: string;
@@ -10,18 +8,7 @@ export interface IWindowStore {
 
 export const defaultWindowProps: IWindowStore = {
     currentFocused: '',
-    activeApps: [
-        // {
-        //     guid: '1',
-        //     name: LocaleKey.powerPrezz,
-        //     openOrder: 1,
-        //     appletType: AppletType.powerPrezz,
-        //     meta: {
-        //         slides: 'EntelectTechAndBorrel2022-work-life',
-        //         isMaximised: true,
-        //     } as any,
-        // }
-    ],
+    activeApps: [],
 };
 
 export const WindowStore = new Store<IWindowStore>(defaultWindowProps);
